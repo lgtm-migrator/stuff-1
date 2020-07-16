b990668c93b865b6e96b7d3a8572d89245c39205
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2020-07-16 19:27:39 +0530

    Add Ability To Play Multiple URLs:   web_stuff/invidious.py

--- web_stuff/invidious.py
+++ web_stuff/invidious.py
@@ -3,11 +3,12 @@ import datetime
 import json
 import mpv
 
-player = mpv.MPV(ytdl=True,
-                 input_default_bindings=True,
-                 input_vo_keyboard=True,
-                 vid=False,
-                 osc=True)
+player = mpv.MPV(
+    ytdl=True,
+    #    vid=False,
+    input_default_bindings=True,
+    input_vo_keyboard=True,
+    osc=True)
 
 
 def join(data):
@@ -15,22 +16,28 @@ def join(data):
     return joined
 
 
+def get_json(url):
+    url = json.loads(urllib.request.urlopen(url).read())
+    return url
+
+
 def length(seconds):
     length = datetime.timedelta(seconds=seconds)
     return length
 
 
-def play(*urls):
-    for url in urls:
-        player.play(url)
-        player.wait_for_playback()
+def play(url):
+    player.play(url)
+    player.wait_for_playback()
 
 
 def get_data(term):
     term = "+".join(term.split())
     data = ["videoId", "title", "lengthSeconds"]
-    url = f"https://invidio.us/api/v1/search?q={term}&fields={join(data)}"
-    content = json.loads(urllib.request.urlopen(url).read())
+    url = "https://invidio.us/api/v1/search?q="\
+        f"{term}&fields={join(data)}"
+
+    content = get_json(url)
     count = 0
     video_id = []
     for i in content:
@@ -41,26 +48,31 @@ def get_data(term):
     return video_id
 
 
-def video_playback(video_id):
+def video_playback(video_ids):
     data = ["hlsUrl", "formatStreams"]
-    video = f"https://invidio.us/api/v1/videos/{video_id}?fields={join(data)}"
-    stream_url = json.loads(urllib.request.urlopen(video).read())
-    try:
-        url = stream_url[data[1]][1]["url"]
-    except IndexError:
+    for video_id in video_ids:
+        url = "https://invidio.us/api/v1/videos/"\
+            f"{video_id}?fields={join(data)}"
+
+        stream_url = get_json(url)
         try:
-            url = stream_url[data[1]][0]["url"]
+            url = stream_url[data[1]][1]["url"]
         except IndexError:
-            url = stream_url[data[0]]
-    play(url)
+            try:
+                url = stream_url[data[1]][0]["url"]
+            except IndexError:
+                url = stream_url[data[0]]
+        play(url)
 
 
 while True:
-    video_id = get_data(input("Search: "))
-    try:
-        choice = int(input("> "))
-    except ValueError:
-        continue
-    if not choice > 20 and not choice < 1:
-        video_playback(video_id[choice - 1])
+    video_ids = get_data(input("Search: "))
+    choice = input("> ").split()
+    choice_list = []
+    for item in choice:
+        item = int(item) - 1
+        if not item > 19 and not item < 0:
+            choice_list.append(item)
+    video_ids = [video_ids[i] for i in choice_list]
+    video_playback(video_ids)
     print(r"End Of Queue ¯\_(ツ)_/¯")
