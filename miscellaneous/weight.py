9fa933acbbb4af15afc3386f20335d9ae7f0b43d
lwritebadcode <67585967+lwritebadcode@users.noreply.github.com>
2020-07-21 09:41:55 +0000

    Update weight.py

--- miscellaneous/weight.py
+++ miscellaneous/weight.py
@@ -1,17 +1,15 @@
-weight = int(input("What is your weight ? : "))
-unit = input("(L)bs or (K)gs : ")
-
-
 def weight_convert(weight, unit):
     if unit.upper() == "L":
         final_weight = weight * 0.45
-        unit = 'kilos'
-
-    if unit.upper() == "K":
+        unit = "kilos"
+    elif unit.upper() == "K":
         final_weight = weight // 0.45
-        unit = 'pounds'
+        unit = "pounds"
     output = f"You are {final_weight} {unit}"
     return output
 
 
-print(weight_convert(weight, unit))
+if __name__ == "__main__":
+    weight = int(input("Weight: "))
+    unit = input("(L)bs or (K)gs: ")
+    print(weight_convert(weight, unit))
