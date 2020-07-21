59a8dd6022e84857fcbc4203d7210b85d856727d
lwritebadcode <67585967+lwritebadcode@users.noreply.github.com>
2020-07-21 09:35:23 +0000

    Update numbers_to_words.py

--- miscellaneous/numbers_to_words.py
+++ miscellaneous/numbers_to_words.py
@@ -1,20 +1,21 @@
 def number_words(numbers):
     number_convert = {
-        '0': 'zero',
-        '1': 'one',
-        '2': 'two',
-        '3': 'three',
-        '4': 'four',
-        '5': 'five',
-        '6': 'six',
-        '7': 'seven',
-        '8': 'eight',
-        '9': 'nine'
+        "0": "zero",
+        "1": "one",
+        "2": "two",
+        "3": "three",
+        "4": "four",
+        "5": "five",
+        "6": "six",
+        "7": "seven",
+        "8": "eight",
+        "9": "nine"
     }
     output = []
     for number in numbers:
         output.append(number_convert.get(number, '!'))
-    return output
+    print(" ".join(output))
 
 
-print(number_words(input('Numbers : ').replace(" ", "")))
+if __name__ == "__main__":
+    number_words(input("Numbers: "))
