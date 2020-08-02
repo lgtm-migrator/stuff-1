be620a1d70ef7758e09dbad3dc3521d5cf9dc782
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2020-08-02 14:33:23 +0530

    new file:   miscellaneous/currency_converter.py

---
+++ miscellaneous/currency_converter.py
@@ -0,0 +1,31 @@
+import urllib.request
+import json
+
+
+def download(url):
+    content = urllib.request.urlopen(url).read()
+    content = json.loads(content)
+    return content
+
+
+def convert(data, currency, value):
+    currency = currency.upper()
+    base_currency = data["base"]
+    conversion_rate = data["rates"][currency]
+    converted_amount = int(value) * conversion_rate
+    result = "{} {} converted from {} at the conversion rate of {}".format(
+        converted_amount, currency, base_currency, conversion_rate)
+    print(result)
+
+
+def main():
+    api_key = "API"
+    url = "http://data.fixer.io/api/latest?access_key={}&format=1".format(
+        api_key)
+    content = download(url)
+    print("Converting from {}".format(content["base"]))
+    convert(content, input("Currency : "), input("Value : "))
+
+
+if __name__ == "__main__":
+    main()
